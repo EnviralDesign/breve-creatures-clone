d36e1f06c1dca48f30752ad106f4cd08c79cf9f5
LUCAS M MORGAN <enviraldesign@gmail.com>
2026-02-24 20:47:19 -0600

    oops

--- src/main.rs
+++ src/main.rs
@@ -8959,7 +8959,7 @@ fn insert_box_body(
     let body = RigidBodyBuilder::dynamic()
         .translation(center)
         .linear_damping(0.19)
-        .angular_damping(8)
+        .angular_damping(8.0)
         .ccd_enabled(true)
         .build();
     let handle = bodies.insert(body);
